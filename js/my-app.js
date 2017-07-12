8f26c19015fd9a71574669fd3a54a0f019cd6a81
Neko Ntsane <nekontsane@gmail.com>
2017-07-12 12:36:53 +0200

    Add Post page and code to view data on it

--- js/my-app.js
+++ js/my-app.js
@@ -18,6 +18,22 @@ myApp.onPageInit('about', function (page) {
     });
 });
 
+//Get parameters from URL
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
 // Generate dynamic page
 var dynamicPageIndex = 0;
 function createContentPage() {
@@ -49,7 +65,7 @@ function createContentPage() {
 
 
 
- var rootURL = 'http://www.informativenews.co.ls/wp-json/wp/v2';
+ var rootURL = 'https://www.selibeng.com/wp-json/wp/v2';
 
  $.ajax({
             // type: 'GET',
@@ -58,34 +74,67 @@ function createContentPage() {
             success: function(data){
                 
                 $.each(data, function(index, value) {
-                  $$('.content-block').append('<div class="card ks-facebook-card">' +
+                  $$('.content-block-main').append('<div class="card ks-facebook-card">' +
                   	'<div class="card-header">' +
                         '<div class="ks-facebook-avatar"><img src="img/selibeng.png" width="34" height="34"/></div>' +
-                        '<div class="ks-facebook-name">Selibeng.com</div>' +
+                        '<div class="ks-facebook-name">Sel<div></div>ibeng.com</div>' +
                         '<div class="ks-facebook-date">'+value.date+'</div>' +
                       '</div>' +
                       '<div class="card-content">' + 
                         '<div class="card-content-inner">' +
                          '<p>'+value.title.rendered+'</p>' +
-                          // '<p class="color-gray">Views: '+value.link+'</p>' +
+                          //'<p class="more-content">Views: '+value.link+'</p>' +
                         '</div>' +
                       '</div>' +
                       '<div class="card-footer">' +
                       '<a href="www.selibeng.com/mis-specialist-at-psi/" class="link">Mark</a>' +
                       '<a href="www.selibeng.com/mis-specialist-at-psi/" class="link">Remove</a>' +
-                      '<a href="https://www.selibeng.com/mis-specialist-at-psi/" class="link external">View</a></div>' +
+                      '<a href="posts.html?postid='+value.id+'" class="link external">View</a></div>' +
                     '<div class="item-inner"><div class="item-title"></div>');
-                  console.log(value.title);
+                  //console.log(value.title.rendered);
+                  //console.log(value.id);
                 });
             },
             error: function(error){
-                $$('.content-block').append('<div class="item-content">' + 
+                $$('.content-block-main').append('<div class="item-content">' + 
                     '<div class="item-title"><div class="item-media"><i class="icon icon-f7"></i></div> there is a problem loading data, you might wanna check you internet connection and retry.</div>');
                 console.log(error);
             }
 
  });
 
- $$('.external').on('click', function () {
-        createContentPage();
-    });
+ 
+ 	var postid = getUrlParameter('postid');
+ 	if (postid != null){
+         $.ajax({
+            // type: 'GET',
+            url: rootURL + '/posts/'+postid,
+            // dataType: 'json',
+            success: function(data){
+           
+                  $$('.post-content-block').append('<div class="card ks-facebook-card">' +
+                  	'<div class="card-header">' +
+                        '<div class="ks-facebook-avatar"><img src="img/selibeng.png" width="34" height="34"/></div>' +
+                        '<div class="ks-facebook-name">Selibeng.com</div>' +
+                        '<div class="ks-facebook-date">'+data.date+'</div>' +
+                      '</div>' +
+                      '<div class="card-content">' + 
+                        '<div class="card-content-inner">' +
+                        '<p><h3>'+data.title.rendered+'</h3></p>' +
+                         '<p>'+data.content.rendered+'</p>' +
+                         '<p>More Content: <a href="'+data.link+'">'+data.link+'</a></p>' +
+                          // '<p class="color-gray">Views: '+value.link+'</p>' +
+                        '</div>' +
+                      '</div>' +
+                    '<div class="item-inner"><div class="item-title"></div>');
+                  //console.log(value.title);
+                
+            },
+            error: function(error){
+                $$('.post-content-block').append('<div class="item-content">' + 
+                    '<div class="item-title"><div class="item-media"><i class="icon icon-f7"></i></div> there is a problem loading data, you might wanna check you internet connection and retry.</div>');
+                console.log(error);
+            }
+
+ 	});
+};
